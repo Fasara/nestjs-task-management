c54f6af40dea4176b74bd1e45647738ec6de84fa
Fabiana <fabiana.asara@gmail.com>
2022-01-05 16:12:35 +0000

    Adds typeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    TasksModule
+  ],
   controllers: [],
   providers: [],
 })
