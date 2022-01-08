0ee72907a657ec08e9a70cff5426a90aec7ad4c2
Fabiana <fabiana.asara@gmail.com>
2022-01-08 18:21:53 +0000

    Adds query for filter

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -7,7 +7,20 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
 	async getAllTask(filterDto: GetTasksFilterDto): Promise<Task[]> {
+		const { status, search } = filterDto;
+
 		const query = this.createQueryBuilder('task');
+
+		if (status) {
+			query.andWhere('task.status = :status', { status });
+		}
+
+		if (search) {
+			query.andWhere(
+				'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+				{ search: `%${search}%` },
+			);
+		}
 
 		const tasks = await query.getMany();
 		return tasks;
