96e4b057ecd65aa28719cd91f680ac3461f3195e
Fabiana <fabiana.asara@gmail.com>
2022-01-14 21:23:29 +0000

    Adds some unit testing

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,22 @@
+import { Test } from '@nestjs/testing';
+import { TasksModule } from './tasks.module';
+import { TasksRepository } from './tasks.repository';
+import { TasksService } from './tasks.service';
+
+const mockTasksRepository = () => ({});
+
+
+describe('TasksService', () => {
+    let tasksService : TasksService;
+    let TasksRepository : TasksRepository;
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [TasksModule,
+            // { provide: TasksRepository,
+            // useFactory: mockTasksRepository }
+            
+            ],
+        })
+    })
+}) 
