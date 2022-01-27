de242b5705021445f21ff1ff618c3376c0a93592
Fabiana <fabiana.asara@gmail.com>
2022-01-27 20:37:36 +0000

    Dammit typo!!! Creates an user table to join to the tasks

--- src/users/dto/user.entity.ts
+++ src/users/dto/user.entity.ts
@@ -1,14 +1,14 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity({ name: 'User' })
 export class User {
     @PrimaryGeneratedColumn()
     id: string;
 
-    @Column({name: 'first-name'})
+    @Column({name: 'first_name'})
     firstName: string;
 
-    @Column({name: 'last-name'})
+    @Column({name: 'last_name'})
     lastName: string;
 
 }
