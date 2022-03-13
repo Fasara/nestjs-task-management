24dcf5d40b58ab6d2d8b8bed6398f550a0e2ec83
Fabiana <fabiana.asara@gmail.com>
2022-03-13 16:48:12 +0000

    Adds auth module - wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     TasksModule,
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
