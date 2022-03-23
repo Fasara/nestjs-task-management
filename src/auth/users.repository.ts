7c94f1dcd84e1fcb3840fc59686ed787c68e2be7
Fabiana <fabiana.asara@gmail.com>
2022-03-23 08:04:14 +0000

    Stores password using bcrypt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,14 +1,21 @@
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
+import * as bcrypt from 'bcrypt';
 
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise <void> {
         const { username, password } = authCredentialsDto;
-        const user = this.create({ username, password });
 
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+        console.log('salt', salt);
+        console.log('hash', hashedPassword);
+
+
+        const user = this.create({ username, password: hashedPassword });
         // wip - needs {unique: true} to work in the entity first
         try {
             await this.save(user);
