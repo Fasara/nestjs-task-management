158d28cb763a56bac0affe756c15acd27393bc80
Fabiana <fabiana.asara@gmail.com>
2022-11-29 21:20:40 +0000

    don't need you

--- src/tasks/tasks.repository.ts
+++
@@ -1,7 +0,0 @@
-// import { Repository } from 'typeorm';
-// import { AppDataSource } from 'data-source';
-// import { Task } from './dto/task.entity';
-// import { CreateTaskDto } from './dto/create-tasks.dto';
-// import { TaskStatus } from './task-status.enum';
-// import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-// export class TasksRepository extends Repository<Task> {}
