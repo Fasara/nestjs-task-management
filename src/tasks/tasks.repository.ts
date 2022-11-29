2da366eabd6c7e9c635107e82ea917ae1d50e1d0
Fabiana <fabiana.asara@gmail.com>
2022-11-29 20:51:35 +0000

    Removes entity repository cos deprecated

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -3,8 +3,6 @@ import { Task } from './dto/task.entity';
 import { CreateTaskDto } from './dto/create-tasks.dto';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-
-@EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
   async getAllTask(filterDto: GetTasksFilterDto): Promise<Task[]> {
     const { status, search } = filterDto;
